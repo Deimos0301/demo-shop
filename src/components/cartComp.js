16a7311714d662bdf93736dc3129955f1b0929ba
Евгений Холопов <deimos0301@gmail.com>
2023-02-17 09:55:35 +0300

    добавил комментарии

--- src/components/cartComp.js
+++ src/components/cartComp.js
@@ -20,8 +20,18 @@ class CartComp extends Component {
         super(props);
 
         this.state = {
-            total: 0,
-            basketData: []
+            total: 0, // Сумма к оплате
+            basketData: [] // Массив, содержащий данные корзины:
+            /*
+                basket_id,
+                product_id,
+                product_name,
+                produc_image_short,
+                quantity,
+                price,
+                product_articul,
+                brand_name
+            */
         };
     }
 
@@ -47,31 +57,36 @@ class CartComp extends Component {
     }
 
     getTotal = (data) => {
+        // По массиву вычисляем сумму к оплате, умножая цену на количество
         return data.reduce((sum, current) => sum + (current.price * current.quantity), 0);
     }
 
     deleteItem = async (e) => {
         if (!window.confirm("Удалить товар из корзины?")) return;
+
+        // Делаем копию массива basketData
         let data = [...this.state.basketData];
+
+        // Ищем индекс элемента в массиве по по basket_id, переданного через props в кнопку "Удалить"
         const idx = data.findIndex(item => item.basket_id === e.component._props.basket_id);
+
+        // Оператор splice удаляет один элемент с индексом idx из массива data
         data.splice(idx, 1);
         await this.basketDelete(e.component._props.basket_id);
         this.setState({ basketData: data });
     }
 
     componentDidMount = async () => {
+        // Получаем с сервера данные корзины
         const arr = await this.getBasket();
         
+        // Отрисовываем список List и сумму к оплате
         this.setState({ basketData: arr, total: this.getTotal(arr) });
-        // console.log(this.props.userInfo);
     }
 
     priceRender = (e) => {
         const price = e.data.price;
         return <div style={{ fontFamily: 'Courier New', fontWeight: "700", fontSize: "18px" }}>{formatter.format(price.toFixed([0]))}</div>
-    }
-    renderGroup = (data) => {
-        return <p style={{ color: "maroon", fontSize: '18px' }}>{data.displayValue}</p>;
     }
 
     onMinusClick = async (e) => {
@@ -85,12 +100,25 @@ class CartComp extends Component {
     }
 
     onPlusClick = async (e) => {
+        // Делаем копию массива basketData. (...) - оператор spread развертывает массив
+        // Без spread мы бы имели не копию массива, а один массив внутри другого
         let data = [...this.state.basketData];
+
+        // Ищем индекс элемента в массиве по по basket_id, переданного через props в кнопку (+)
         const idx = data.findIndex(item => item.basket_id === e.component._props.basket_id);
 
+        if (idx < 0) return;
+        
+        // В найденном элемента массива увеличиваем количество товаров на 1
         data[idx].quantity += 1;
 
+        // Записывам изменения в БД
         await this.basketUpdate(e.component._props.basket_id, data[idx].quantity);
+
+        // Отображаем сделанные изменения на странице
+        // Так ка наш List построен на источнике this.state.basketData,
+        // то именно в него мы и "записываем" изменения из копии массива data
+        // Заодно здесь же меняем и итоговую сумму к оплате
         this.setState({ basketData: data, total: this.getTotal(data) });
     }
 
@@ -114,6 +142,9 @@ class CartComp extends Component {
         });
     }
 
+    onQuantityChanged = (e) => {
+        alert('Изменили значения вручную')
+    }
 
     cartItem = (e) => {
         const href = `/product?product_id=${e.product_id}`;
@@ -134,10 +165,12 @@ class CartComp extends Component {
                     </div>
 
                     <NumberBox
-                        width="50px"
+                        basket_id={e.basket_id}
+                        width="55px"
                         defaultValue={1}
                         value={e.quantity}
                         min={1}
+                        onValueChanged={this.onQuantityChanged}
                         showSpinButtons={false}
                     />
 
@@ -153,41 +186,6 @@ class CartComp extends Component {
     render() {
         return (
             <div className='cart-wrap'>
-                {/* <DataGrid
-                    dataSource={this.state.basketData}>
-                    <Column
-                        dataField='product_image_short'
-                        allowFiltering={false}
-                        allowSorting={false}
-                        cellRender={this.getSmallImage}
-                        width={170}>
-                    </Column>
-                    <Column
-                        dataField='product_name'
-                        width={350}>
-                    </Column>
-                    <Column
-                        dataField='total'
-                        width={100}>
-                        <div style={{ display: "flex", marginLeft: "5px", justifyContent: "center", alignItems: "center" }}>
-                            <div><Button icon="minus" onClick={this.onMinusClick} disabled={this.state.minusDisabled} /></div>
-                            <NumberBox
-                                width="50px"
-                                defaultValue={1}
-                                value={this.state.total}
-                                min={1}
-                                showSpinButtons={false} />
-                            <div><Button icon="plus" onClick={this.onPlusClick} /></div>
-                        </div>
-                    </Column>
-                    <Column
-                        dataField='summa'
-                        cellRender={this.priceRender}
-                        width={150}
-                        format=",##0">
-                    </Column>
-                </DataGrid> */}
-
 
                 <List
                     dataSource={this.state.basketData}
@@ -195,7 +193,7 @@ class CartComp extends Component {
                     repaintChangesOnly={false}>
                 </List>
 
-                <div style={{ fontSize: "16px", fontWeight: "600" }}>Итого: {formatter.format(this.state.total.toFixed([0]))} </div>
+                <div style={{ fontSize: "16px", fontWeight: "600", marginRight: "10px", textAlign: "end" }}>К оплате: {formatter.format(this.state.total.toFixed([0]))} </div>
             </div>
         );
     }
